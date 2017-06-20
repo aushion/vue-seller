4d26d122a21af098f53d5efa8581715665e62521
aushion <chenaosheng@163.com>
2017-06-20 23:53:06 +0800

    添加eslint分号支持

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   'rules': {
     // allow paren-less arrow functions
     'eol-last': 0,
+    "no-extra-semi":0,
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
