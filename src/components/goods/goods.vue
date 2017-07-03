3632aa886349436b29f0d67ebc21ed6335bc739b
aushion <chenaosheng@163.com>
2017-07-03 23:36:54 +0800

    some style

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,15 +2,40 @@
     <div class="goods">
       <div class="menu-wrapper">
         <ul>
-          <li v-for="item in goods" class="menu-item">
-            <span class="text border-1px">
+          <li v-for="item in goods" class="menu-item border-1px">
+            <span class="text">
               <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
               {{item.name}}
             </span>
           </li>
         </ul>
       </div>
-      <div class="foods-wrapper"></div>
+      <div class="foods-wrapper">
+        <ul>
+          <li v-for="item in goods" class="food-list">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li v-for="food in item.foods" class="food-item border-1px">
+                <div class="icon">
+                  <img :src="food.icon">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span>月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span>￥{{food.price}}</span>
+                    <span v-show="food.oldPrice">{{food.oldPrice}}</span>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
 </template>
 
@@ -40,7 +65,7 @@
 </script>
 
 <style lang="stylus">
-  @import '../../common/stylus/mixin.styl';
+  @import '../../common/stylus/mixin.styl'
   .goods
     display: flex
     position: absolute
@@ -57,6 +82,8 @@
         height: 56px
         width: 54px
         line-height: 14px
+        padding: 0 12px
+        border-1px(rgba(7,17,27,0.1))
         .icon
           display: inline-block
           vertical-align : top
@@ -81,7 +108,23 @@
           font-size 12px
           line-height 14px
           width: 54px
-          border-1px(rgba(7,17,27,.1))
     .foods-wrapper
       flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147,153,159)
+        background: #f3f5f7
+        .food-item
+          display flex
+          margin: 18px
+          padding-bottom: 18px
+          border-1px(rgba(7,17,27,0.1))
+          &:last-child
+            border-none()
+            margin-bottom: 0
+
 </style>
